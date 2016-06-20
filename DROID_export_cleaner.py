754a7b1f0451948c50eb50ab57faf35e2ec854da
jayGattusoNLNZ <jay.gattuso@dia.govt.nz>
2016-06-21 07:58:35 +1200

    Add files via upload

--- DROID_export_cleaner.py
+++ DROID_export_cleaner.py
@@ -3,16 +3,17 @@ import os
 
 
 #### make sure this filename is the same as the one on your system
-csv_filename = r"thread0.csv"
+input_csv_filename = r"thread0.csv"
+output_csv_filename = "cleaned_csv.csv"
 
 ########### do not edit below this line #################
 
-if not os.path.exists(csv_filename):
-	quit("Can't find the log file '{}', please check your filename".format(csv_filename))
+if not os.path.exists(input_csv_filename):
+	quit("Can't find the log file '{}', please check your filename".format(input_csv_filename))
 
 cleaned_csv_data = []
 
-with open(csv_filename) as data:
+with open(input_csv_filename) as data:
 	csv_reader = csv.reader(data)
 	for row in csv_reader:
 		if row[8] != "Folder":
@@ -22,7 +23,7 @@ headers = cleaned_csv_data[0]
 cleaned_csv_data.remove(headers)
 cleaned_csv_data.sort()
 
-with open("cleaned_csv.csv", "wb") as data:
+with open(output_csv_filename, "wb") as data:
 	writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 	writer.writerow(headers)
 	for row in cleaned_csv_data:
